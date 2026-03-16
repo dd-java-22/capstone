e02419965ea038fd1c91808fdd5c84e01a5d84b6
Skylar <sscott28@cnm.edu>
2026-03-15 21:08:06 -0600

    swapped dependency on googleid and credentials instead of auth

--- app/src/main/java/edu/cnm/deepdive/seesomethingabq/service/repository/GoogleAuthRepository.kt
+++ app/src/main/java/edu/cnm/deepdive/seesomethingabq/service/repository/GoogleAuthRepository.kt
@@ -1,4 +1,10 @@
 package edu.cnm.deepdive.seesomethingabq.service.repository
 
+import android.app.Activity
+import java.util.concurrent.CompletableFuture
+
 interface GoogleAuthRepository {
+
+    fun signInQuickly(activity: Activity): CompletableFuture<GoogleIdTokenCredential>
+
 }
