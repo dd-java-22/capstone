c5f4f916e2a0b21bfbf4fa7057a2135695006cfe
Skylar <sscott28@cnm.edu>
2026-03-17 00:55:29 -0600

    copied in rest of demo app, still debugging.
    lots of config differences that didn't run. Now stuck debugging login credential issue crash. Can't get the credential from the credentialManager. maybe a setup issue, maybe an injection issue...?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,8 @@ android {
             "de.mannodermaus.junit5.AndroidJUnit5Builder"
 
         resValue("string", "app_name", project.property("appName") as String)
-//        resValue("string", "client_id", getLocalProperty("clientId") as String)
+        resValue("string", "client_id", getLocalProperty("clientId") as String)
+
 
         javaCompileOptions {
             annotationProcessorOptions {
