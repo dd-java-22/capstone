e83ff9d5b4511674fcaced7f597ca42866d24ac7
Skylar <sscott28@cnm.edu>
2026-03-18 09:29:54 -0600

    Stub repository interfaces added in repository package.

---
+++ server/src/main/java/edu/cnm/deepdive/seesomethingabq/service/repository/IssueReportRepository.java
@@ -0,0 +1,10 @@
+package edu.cnm.deepdive.seesomethingabq.service.repository;
+
+import edu.cnm.deepdive.seesomethingabq.model.entity.IssueReport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IssueReportRepository extends JpaRepository<IssueReport, Long> {
+
+
+
+}
