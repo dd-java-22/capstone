1a30fd351e6360766452f8189ede9d6382610363
Skylar <sscott28@cnm.edu>
2026-04-07 15:13:34 -0600

    allow receiving of page data by actually ``@Expose`ing the DTOs' fields

--- app/src/main/java/edu/cnm/deepdive/seesomethingabq/model/dto/IssueReportSummary.kt
+++ app/src/main/java/edu/cnm/deepdive/seesomethingabq/model/dto/IssueReportSummary.kt
@@ -1,12 +1,18 @@
 package edu.cnm.deepdive.seesomethingabq.model.dto
 
+import com.google.gson.annotations.Expose
 import java.time.Instant
 import java.util.UUID
 
 data class IssueReportSummary (
+    @Expose
     val externalId: UUID,
+    @Expose
     val description: String = "",
+    @Expose
     val acceptedState: String = "",
+    @Expose
     val timeFirstReported: Instant = Instant.EPOCH,
+    @Expose
     val timeLastModified: Instant = Instant.EPOCH,
 )
--- app/src/main/java/edu/cnm/deepdive/seesomethingabq/model/dto/PaginatedResponse.kt
+++ app/src/main/java/edu/cnm/deepdive/seesomethingabq/model/dto/PaginatedResponse.kt
@@ -1,13 +1,22 @@
 package edu.cnm.deepdive.seesomethingabq.model.dto
 
+import com.google.gson.annotations.Expose
+
 
 // Written by Gemini
 data class PaginatedResponse<T>(
+    @Expose
     val content: List<T>,          // This is your list of Summary objects
+    @Expose
     val totalElements: Long,
+    @Expose
     val totalPages: Int,
+    @Expose
     val size: Int,
+    @Expose
     val number: Int,               // Current page number
+    @Expose
     val last: Boolean,
+    @Expose
     val first: Boolean
 )
