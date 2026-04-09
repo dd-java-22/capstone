19ff00739b15e975cd4b9e326f722fa53e0ad65a
Kevin Dilts <kdilts@cnm.edu>
2026-04-09 10:19:07 -0600

    Change by Codex: Add manager user detail navigation
    Added a manager user detail destination and Safe Args externalId argument.
    
    Wired ManageUsers row taps to navigate to the new destination.
    
    Prompt: Add navigation from ManageUsers list to a new manager-facing user detail screen using Navigation Component Safe Args with externalId/UUID.

---
+++ app/src/main/java/edu/cnm/deepdive/seesomethingabq/controller/ManagerUserDetailFragment.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright 2026 CNM Ingenuity, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package edu.cnm.deepdive.seesomethingabq.controller;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.seesomethingabq.databinding.FragmentManagerUserDetailBinding;
+import java.util.UUID;
+
+@AndroidEntryPoint
+/**
+ * Fragment displaying manager-facing details for a single user.
+ *
+ * This is a placeholder destination; UI will be filled in later.
+ */
+public class ManagerUserDetailFragment extends Fragment {
+
+  private FragmentManagerUserDetailBinding binding;
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    binding = FragmentManagerUserDetailBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    UUID externalId = ManagerUserDetailFragmentArgs.fromBundle(requireArguments()).getExternalId();
+    binding.externalIdValue.setText(String.valueOf(externalId));
+  }
+
+  @Override
+  public void onDestroyView() {
+    binding = null;
+    super.onDestroyView();
+  }
+
+}
+
